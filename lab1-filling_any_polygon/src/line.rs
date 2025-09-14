a6b676595e9b390b4e0f844bc60d94b9a822d52e
ecarcamo <estebancarcamou@gmail.com>
2025-09-13 23:11:46 -0600

    cargo fmt para lab1

--- lab1-filling_any_polygon/src/line.rs
+++ lab1-filling_any_polygon/src/line.rs
@@ -1,20 +1,16 @@
-use raylib::prelude::*;
 use crate::framebuffer::Framebuffer;
+use raylib::prelude::*;
 
-pub fn line(
-    framebuffer: &mut Framebuffer,
-    start: Vector2,
-    end:   Vector2,
-) {
+pub fn line(framebuffer: &mut Framebuffer, start: Vector2, end: Vector2) {
     let mut x0 = start.x as i32;
     let mut y0 = start.y as i32;
-    let   x1 = end.x   as i32;
-    let   y1 = end.y   as i32;
+    let x1 = end.x as i32;
+    let y1 = end.y as i32;
 
-    let dx =  (x1 - x0).abs();
-    let dy =  (y1 - y0).abs();
-    let sx = if x0 < x1 {  1 } else { -1 };
-    let sy = if y0 < y1 {  1 } else { -1 };
+    let dx = (x1 - x0).abs();
+    let dy = (y1 - y0).abs();
+    let sx = if x0 < x1 { 1 } else { -1 };
+    let sy = if y0 < y1 { 1 } else { -1 };
     let mut err = dx - dy;
 
     loop {
@@ -27,11 +23,11 @@ pub fn line(
         let e2 = err * 2;
         if e2 > -dy {
             err -= dy;
-            x0   += sx;
+            x0 += sx;
         }
-        if e2 <  dx {
+        if e2 < dx {
             err += dx;
-            y0   += sy;
+            y0 += sy;
         }
     }
 }
